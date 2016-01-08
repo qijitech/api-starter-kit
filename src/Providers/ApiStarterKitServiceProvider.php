5f71a105071a41e1fe7d69490bf1cab72f1fb395
杨玉刚 <smartydroid@gmail.com>
2016-01-08 21:57:58 +0800

    add publish config provider

---
+++ src/Providers/ApiStarterKitServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Shared\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiStarterKitServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishConfigs();
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Publish config file.
+     */
+    private function publishConfigs()
+    {
+        $this->publishes([
+            __DIR__ . '/../Config/config.php' => config_path('consts.php'),
+        ]);
+    }
+
+}
