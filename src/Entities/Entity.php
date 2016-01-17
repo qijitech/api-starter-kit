19f26f0831e26290d6a578f288707568c345eab1
杨玉刚 <smartydroid@gmail.com>
2016-01-17 23:11:48 +0800

    add attribute for createAt

--- src/Entities/Entity.php
+++ src/Entities/Entity.php
@@ -6,4 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 class Entity extends Model
 {
 
+    /**
+     * @return int
+     */
+    public function getCreatedAtAttribute()
+    {
+        $time = strtotime($this->attributes['created_at']);
+        return $time == false ? 0 : $time;
+    }
+
 }
