f083190dc5efe28f396f56b7421e4f41472ad01d
杨玉刚 <smartydroid@gmail.com>
2016-02-25 18:01:12 +0800

    add values for enum

--- src/Enums/Enum.php
+++ src/Enums/Enum.php
@@ -9,6 +9,15 @@ abstract class Enum
   /**
    * @return array
    */
+  public static function getValues()
+  {
+    $class = new ReflectionClass(get_called_class());
+    return array_values($class->getConstants());
+  }
+  
+  /**
+   * @return array
+   */
   public static function getKeys()
   {
     $class = new ReflectionClass(get_called_class());
