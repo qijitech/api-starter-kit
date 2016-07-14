dd7e960184f3fb29965c789defec79c3e4818945
杨玉刚 <smartydroid@gmail.com>
2016-07-15 00:24:59 +0800

    add bcrypt func for lumen

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,19 @@
 <?php
 
+if (! function_exists('bcrypt')) {
+  /**
+   * Hash the given value.
+   *
+   * @param  string  $value
+   * @param  array   $options
+   * @return string
+   */
+  function bcrypt($value, $options = [])
+  {
+    return app('hash')->make($value, $options);
+  }
+}
+
 if (!function_exists('trans')) {
   /**
    * Translate the given message.
